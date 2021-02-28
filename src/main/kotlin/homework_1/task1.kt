7422d805161afe80dbd03350a72a5e5e12f0f5cc
ubuntuegor <egorxxx29@gmail.com>
2021-02-28 03:24:30 +0300

    Homework 1, task 1

---
+++ src/main/kotlin/homework_1/task1.kt
@@ -0,0 +1,25 @@
+package homework_1
+
+fun findFactorialIterative(n: Int): Int {
+    var result = 1
+    for (i in 1..n) result *= i
+    return result
+}
+
+fun findFactorialRecursive(n: Int): Int {
+    if (n <= 1) return 1
+    return n * findFactorialRecursive(n - 1)
+}
+
+fun main() {
+    print("Enter an integer: ")
+    val n = readLine()?.toInt()
+
+    if (n == null || n < 1) {
+        println("Failed to read input")
+        return
+    }
+
+    println("Factorial (iterative) of $n: ${findFactorialIterative(n)}")
+    println("Factorial (recursive) of $n: ${findFactorialRecursive(n)}")
+}
