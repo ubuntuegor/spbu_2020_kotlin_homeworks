ac851b2663aa479abbbf2fa929cf0b38a14ecc91
ubuntuegor <egorxxx29@gmail.com>
2021-02-28 03:25:52 +0300

    Homework 1, task 2

---
+++ src/main/kotlin/homework_1/task2.kt
@@ -0,0 +1,26 @@
+package homework_1
+
+fun countOccurrences(needle: String, haystack: String): Int {
+    var occurrences = 0
+    var index = haystack.indexOf(needle)
+    while (index != -1) {
+        index = haystack.indexOf(needle, index + 1)
+        occurrences++
+    }
+    return occurrences
+}
+
+fun main() {
+    print("Enter string to find: ")
+    val needle = readLine()
+    print("Enter string to search in: ")
+    val haystack = readLine()
+
+    if (needle == null || haystack == null) {
+        println("Failed to read input")
+        return
+    }
+
+    val result = countOccurrences(needle, haystack)
+    println("Found $result occurrences")
+}
