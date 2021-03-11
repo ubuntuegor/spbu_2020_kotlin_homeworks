3ceba77884522351628a604c4eff8ac7f6842116
ubuntuegor <egorxxx29@gmail.com>
2021-03-11 21:05:22 +0300

    Homework 2, task 1: initial

--- src/main/kotlin/homework1/actions/AppendToStartAction.kt
+++ src/main/kotlin/homework1/actions/AppendToStartAction.kt
@@ -1,5 +1,8 @@
 package homework1.actions
 
+/**
+ * This action appends to the start of the list.
+ */
 class AppendToStartAction(private val value: Int) : Action {
     override val name: String
         get() = "Append $value to start"
