af4cfadc237cb9a912fddc2db6a293da0b27d4ff
ubuntuegor <egorxxx29@gmail.com>
2021-03-23 20:20:47 +0300

    Simplify JUnit dependency metadata

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,9 +22,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.14.2")
 }
 
