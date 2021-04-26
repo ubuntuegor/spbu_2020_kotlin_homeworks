db62c67e53a0d1de22ec51ab889e39e1003bcddd
ubuntuegor <me@bnt.to>
2021-04-26 05:15:57 +0300

    Homework 6, task 1: add more test cases

--- src/test/kotlin/homework6/MergeRunnableTest.kt
+++ src/test/kotlin/homework6/MergeRunnableTest.kt
@@ -11,12 +11,14 @@ internal class MergeRunnableTest {
         @JvmStatic
         fun mergeData() = listOf(
             Arguments.of(listOf(1, 2, 3, 4), listOf(1, 3), listOf(2, 4)),
+            Arguments.of(listOf(1, 1, 1, 1, 4), listOf(1, 4), listOf(1, 1, 1)),
             Arguments.of(listOf(2, 3, 4), listOf(2, 3, 4), listOf<Int>()),
+            Arguments.of(listOf<Int>(), listOf<Int>(), listOf<Int>()),
         )
     }
 
     @MethodSource("mergeData")
-    @ParameterizedTest
+    @ParameterizedTest(name = "runSync - {0}")
     fun runSync(expected: List<Int>, list1: List<Int>, list2: List<Int>) {
         val options = MergeSorterOptions(false, Semaphore(10))
         val mergeRunnable = MergeRunnable(list1, list2, 0, options)
@@ -25,7 +27,7 @@ internal class MergeRunnableTest {
     }
 
     @MethodSource("mergeData")
-    @ParameterizedTest
+    @ParameterizedTest(name = "runParallel - {0}")
     fun runParallel(expected: List<Int>, list1: List<Int>, list2: List<Int>) {
         val options = MergeSorterOptions(true, Semaphore(10))
         val mergeRunnable = MergeRunnable(list1, list2, 10, options)
--- src/test/kotlin/homework6/MergeSorterTest.kt
+++ src/test/kotlin/homework6/MergeSorterTest.kt
@@ -10,20 +10,21 @@ internal class MergeSorterTest {
         @JvmStatic
         fun sortData() = listOf(
             Arguments.of(listOf(1, 2, 3, 4), listOf(4, 3, 2, 1)),
+            Arguments.of(listOf(1, 2, 2, 2, 3, 4), listOf(4, 2, 3, 2, 1, 2)),
             Arguments.of(listOf('a', 'b', 'c'), listOf('b', 'c', 'a')),
             Arguments.of(listOf<Int>(), listOf<Int>()),
         )
     }
 
     @MethodSource("sortData")
-    @ParameterizedTest
+    @ParameterizedTest(name = "sort - {0}")
     fun <T : Comparable<Any>> sort(expected: List<T>, unsorted: List<T>) {
         val sorter = MergeSorter<T>(10, 10, true)
         assertEquals(expected, sorter.sort(unsorted))
     }
 
     @MethodSource("sortData")
-    @ParameterizedTest
+    @ParameterizedTest(name = "sortSync - {0}")
     fun <T : Comparable<Any>> sortSync(expected: List<T>, unsorted: List<T>) {
         val sorter = MergeSorter<T>(0, 1, true)
         assertEquals(expected, sorter.sort(unsorted))
