e977e70653ec3c672d6724f2f747945e8b0483db
ubuntuegor <me@bnt.to>
2021-05-05 17:15:18 +0300

    Homework 6, task 1: refactor tests

--- src/test/kotlin/homework6/MergeSorterTest.kt
+++ src/test/kotlin/homework6/MergeSorterTest.kt
@@ -8,25 +8,23 @@ import org.junit.jupiter.params.provider.MethodSource
 internal class MergeSorterTest {
     companion object {
         @JvmStatic
-        fun sortData() = listOf(
-            Arguments.of(listOf(1, 2, 3, 4), listOf(4, 3, 2, 1)),
-            Arguments.of(listOf(1, 2, 2, 2, 3, 4), listOf(4, 2, 3, 2, 1, 2)),
-            Arguments.of(listOf('a', 'b', 'c'), listOf('b', 'c', 'a')),
-            Arguments.of(listOf<Int>(), listOf<Int>()),
-        )
+        fun sortData(): List<Arguments> {
+            val arguments = mutableListOf<Arguments>()
+            for (recursionLimit in (0..6)) {
+                arguments.add(Arguments.of(listOf(1, 2, 3, 4), listOf(4, 3, 2, 1), recursionLimit))
+                arguments.add(Arguments.of(listOf(1, 2, 2, 2, 3, 4), listOf(4, 2, 3, 2, 1, 2), recursionLimit))
+                arguments.add(Arguments.of(listOf('a', 'b', 'c'), listOf('b', 'c', 'a'), recursionLimit))
+                arguments.add(Arguments.of(listOf<Int>(), listOf<Int>(), recursionLimit))
+                arguments.add(Arguments.of((1..100_000).toList(), (1..100_000).shuffled(), recursionLimit))
+            }
+            return arguments
+        }
     }
 
     @MethodSource("sortData")
-    @ParameterizedTest(name = "sort - {0}")
-    fun <T : Comparable<Any>> sort(expected: List<T>, unsorted: List<T>) {
-        val sorter = MergeSorter<T>(10, 10, true)
-        assertEquals(expected, sorter.sort(unsorted))
-    }
-
-    @MethodSource("sortData")
-    @ParameterizedTest(name = "sortSync - {0}")
-    fun <T : Comparable<Any>> sortSync(expected: List<T>, unsorted: List<T>) {
-        val sorter = MergeSorter<T>(0, 1, true)
+    @ParameterizedTest(name = "sort - {0} (recursion limit {2})")
+    fun <T : Comparable<Any>> sort(expected: List<T>, unsorted: List<T>, recursionLimit: Int) {
+        val sorter = MergeSorter<T>(recursionLimit, true)
         assertEquals(expected, sorter.sort(unsorted))
     }
 }
