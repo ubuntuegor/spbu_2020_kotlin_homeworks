038e06a2ace83043e5901690934c185fdea0b246
Egor Kukoverov <me@bnt.to>
2021-05-28 05:10:21 +0300

    Homework 8, task 1: forgot to remove this

--- src/main/kotlin/homework8/NoughtsAndCrossesApp.kt
+++ src/main/kotlin/homework8/NoughtsAndCrossesApp.kt
@@ -3,10 +3,4 @@ package homework8
 import homework8.views.MainMenuView
 import tornadofx.App
 
-class NoughtsAndCrossesApp : App(MainMenuView::class, Styles::class) /* {
-    private val controller: ConnectionController by inject()
-
-    override fun stop() {
-        controller.disconnect()
-    }
-} */
+class NoughtsAndCrossesApp : App(MainMenuView::class, Styles::class)
